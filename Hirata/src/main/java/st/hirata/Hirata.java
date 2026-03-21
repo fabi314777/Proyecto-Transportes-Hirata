72e7786698cfccc6781457aa43c2e0d7dde72847
Martin10909 <m.lonconado@alumnos.santotomas.cl>
2026-03-21 20:19:11 -0300

    Se crea el Main

---
+++ Hirata/src/main/java/st/hirata/Hirata.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package st.hirata;
+
+/**
+ *
+ * @author marti
+ */
+public class Hirata {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
